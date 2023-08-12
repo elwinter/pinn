641eeeda18859541a0fc74d05b2155addff8092d
Eric Winter <eric.winter@jhuapl.edu>
2023-08-12 12:14:40 -0400

    Cleaned up, added new hyperparameters.

--- pinn/common.py
+++ pinn/common.py
@@ -21,20 +21,22 @@ import sys
 import numpy as np
 import tensorflow as tf
 
+# Import project modules.
+
 
 # Module constants
 
 # Name of file to hold the system information report.
-system_information_file = "system_information.txt"
+SYSTEM_INFORMATION_FILE = "system_information.txt"
 
 # Name of file to hold the network hyperparameters, as an importable Python
 # module.
-hyperparameter_file = "hyperparameters.py"
+HYPERPARAMETER_FILE = "hyperparameters.py"
 
 # Initial parameter ranges
-w0_range = [-0.1, 0.1]  # Hidden layer weights
-u0_range = [-0.1, 0.1]  # Hidden layer biases
-v0_range = [-0.1, 0.1]  # Output layer weights
+W0_RANGE = [-0.1, 0.1]  # Hidden layer weights
+U0_RANGE = [-0.1, 0.1]  # Hidden layer biases
+V0_RANGE = [-0.1, 0.1]  # Output layer weights
 
 
 def build_model(n_layers, n_hidden, activation):
@@ -59,108 +61,29 @@ def build_model(n_layers, n_hidden, activation):
     -------
     model : tf.keras.Sequential
         The neural network.
+
+    Raises
+    ------
+    None
     """
     layers = []
     for _ in range(n_layers):
         hidden_layer = tf.keras.layers.Dense(
             units=n_hidden, use_bias=True,
             activation=tf.keras.activations.deserialize(activation),
-            kernel_initializer=tf.keras.initializers.RandomUniform(*w0_range),
-            bias_initializer=tf.keras.initializers.RandomUniform(*u0_range)
+            kernel_initializer=tf.keras.initializers.RandomUniform(*W0_RANGE),
+            bias_initializer=tf.keras.initializers.RandomUniform(*U0_RANGE)
         )
         layers.append(hidden_layer)
     output_layer = tf.keras.layers.Dense(
         units=1,
         activation=tf.keras.activations.linear,
-        kernel_initializer=tf.keras.initializers.RandomUniform(*v0_range),
+        kernel_initializer=tf.keras.initializers.RandomUniform(*V0_RANGE),
         use_bias=False,
     )
     layers.append(output_layer)
     model = tf.keras.Sequential(layers)
     return model
-
-
-def save_hyperparameters(args, output_dir):
-    """Save the neural network hyperparameters.
-
-    Print a record of the hyperparameters of the neural network in the
-    specified directory, as an importable python module.
-
-    Parameters
-    ----------
-    args : dict
-        Dictionary of command-line arguments.
-    output_dir : str
-        Path to directory to contain the report.
-
-    Returns
-    -------
-    path : str
-        Path to hyperparameter file.
-    """
-    path = os.path.join(output_dir, hyperparameter_file)
-    with open(path, "w") as f:
-        f.write("activation = %s\n" % repr(args.activation))
-        f.write("convcheck = %s\n" % repr(args.convcheck))
-        f.write("learning_rate = %s\n" % repr(args.learning_rate))
-        f.write("max_epochs = %s\n" % repr(args.max_epochs))
-        f.write("n_hid = %s\n" % repr(args.n_hid))
-        f.write("n_layers = %s\n" % repr(args.n_layers))
-        f.write("precision = %s\n" % repr(args.precision))
-        f.write("random_seed = %s\n" % repr(args.seed))
-        f.write("tolerance = %s\n" % repr(args.tolerance))
-        f.write("w_data = %s\n" % repr(args.w_data))
-    return path
-
-
-def save_problem_definition(problem, output_dir):
-    """Save the problem definition for the run.
-
-    Copy the problem definition file to the output directory.
-
-    Parameters
-    ----------
-    problem : module
-        Imported module object for problem definition.
-    output_dir : str
-        Path to directory to contain the copy of the problem definition file.
-
-    Returns
-    -------
-    None
-    """
-    # Copy the problem definition file to the output directory.
-    shutil.copy(problem.__file__, output_dir)
-
-
-def save_system_information(output_dir):
-    """Save a summary of system characteristics.
-
-    Save a summary of the host system in the specified directory.
-
-    Parameters
-    ----------
-    output_dir : str
-        Path to directory to contain the report.
-
-    Returns
-    -------
-    None
-    """
-    path = os.path.join(output_dir, system_information_file)
-    with open(path, "w") as f:
-        f.write("System report:\n")
-        f.write("Start time: %s\n" % datetime.datetime.now())
-        f.write("Host name: %s\n" % platform.node())
-        f.write("Platform: %s\n" % platform.platform())
-        f.write("uname: " + " ".join(platform.uname()) + "\n")
-        f.write("Python version: %s\n" % sys.version)
-        f.write("Python build: %s\n" % " ".join(platform.python_build()))
-        f.write("Python compiler: %s\n" % platform.python_compiler())
-        f.write("Python implementation: %s\n" %
-                platform.python_implementation())
-        f.write("NumPy version: %s\n" % np.__version__)
-        f.write("TensorFlow version: %s\n" % tf.__version__)
 
 
 def find_last_epoch(results_path):
@@ -177,6 +100,10 @@ def find_last_epoch(results_path):
     -------
     last_epoch : int
         Number for last epoch found in results directory.
+
+    Raises
+    ------
+    None
     """
     # Save the current directory.
     original_directory = os.getcwd()
@@ -200,6 +127,102 @@ def find_last_epoch(results_path):
 
     # Return the largest epoch number.
     return last_epoch
+
+
+def save_hyperparameters(args, output_dir):
+    """Save the neural network hyperparameters.
+
+    Print a record of the hyperparameters of the neural network in the
+    specified directory, as an importable python module.
+
+    Parameters
+    ----------
+    args : dict
+        Dictionary of command-line arguments.
+    output_dir : str
+        Path to directory to contain the report.
+
+    Returns
+    -------
+    path : str
+        Path to hyperparameter file.
+
+    Raises
+    ------
+    None
+    """
+    path = os.path.join(output_dir, HYPERPARAMETER_FILE)
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(f"activation = {repr(args.activation)}\n")
+        f.write(f"batch_size = {repr(args.batch_size)}\n")
+        f.write(f"data = {repr(args.data)}\n")
+        f.write(f"learning_rate = {repr(args.learning_rate)}\n")
+        f.write(f"max_epochs = {repr(args.max_epochs)}\n")
+        f.write(f"n_hid = {repr(args.n_hid)}\n")
+        f.write(f"n_layers = {repr(args.n_layers)}\n")
+        f.write(f"precision = {repr(args.precision)}\n")
+        f.write(f"random_seed = {repr(args.seed)}\n")
+        f.write(f"validation = {repr(args.validation)}\n")
+        f.write(f"w_data = {repr(args.w_data)}\n")
+        f.write(f"problem_path = {repr(args.problem)}\n")
+        f.write(f"training_points_path = {repr(args.training_points)}\n")
+    return path
+
+
+def save_problem_definition(problem, output_dir):
+    """Save the problem definition for the run.
+
+    Copy the problem definition file to the output directory.
+
+    Parameters
+    ----------
+    problem : module
+        Imported module object for problem definition.
+    output_dir : str
+        Path to directory to contain the copy of the problem definition file.
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    None
+    """
+    # Copy the problem definition file to the output directory.
+    shutil.copy(problem.__file__, output_dir)
+
+
+def save_system_information(output_dir):
+    """Save a summary of system characteristics.
+
+    Save a summary of the host system in the specified directory.
+
+    Parameters
+    ----------
+    output_dir : str
+        Path to directory to contain the report.
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    None
+    """
+    path = os.path.join(output_dir, SYSTEM_INFORMATION_FILE)
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(f"Start time: {datetime.datetime.now()}\n")
+        f.write(f"Host name: {platform.node()}\n")
+        f.write(f"Platform: {platform.platform()}\n")
+        f.write(f"uname: {' '.join(platform.uname())}\n")
+        f.write(f"Python version: {sys.version}\n")
+        f.write(f"Python build: {' '.join(platform.python_build())}\n")
+        f.write(f"Python compiler: {platform.python_compiler()}\n")
+        f.write(f"Python implementation: {platform.python_implementation()}\n")
+        f.write(f"NumPy version: {np.__version__}\n")
+        f.write(f"TensorFlow version: {tf.__version__}\n")
 
 
 if __name__ == '__main__':
