bcc2ecb73fea9f1f63385ccb1976e06638765f2c
Eric Winter <eric.winter@jhuapl.edu>
2023-11-19 12:24:55 -0500

    Updated pinn2.py, added lagaris03 problem.

---
+++ problems/lagaris/lagaris03/lagaris03_create_data.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+"""Create data for lagaris02 problem.
+
+This is for problem 3 from Lagaris (1998).
+
+Author
+------
+eric.winter62@gmail.com
+"""
+
+
+# Import standard Python modules.
+import argparse
+
+# Import supplemental Python modules.
+import numpy as np
+
+# Import project Python modules.
+import problems.lagaris.lagaris03.lagaris03 as p
+
+
+# Program constants
+
+# Program description.
+description = "Create data for lagaris03 problem."
+
+
+def create_command_line_argument_parser():
+    """Create the command-line argument parser.
+
+    Create the command-line argument parser.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    parser : argparse.ArgumentParser
+        Parser for command-line arguments.
+
+    Raises
+    ------
+    None
+    """
+    parser = argparse.ArgumentParser(description)
+    parser.add_argument(
+        "--debug", "-d", action="store_true",
+        help="Print debugging output (default: %(default)s)."
+    )
+    parser.add_argument("rest", nargs=argparse.REMAINDER)
+    return parser
+
+
+def main():
+    """Begin main program."""
+    # Set up the command-line parser.
+    parser = create_command_line_argument_parser()
+
+    # Parse the command-line arguments.
+    args = parser.parse_args()
+    if args.debug:
+        print(f"args = {args}")
+    debug = args.debug
+    rest = args.rest
+
+    # Fetch the remaining command-line arguments.
+    # They should be in a set of 3:
+    # x_min x_max n_x
+    assert len(rest) == 3
+    x_min = float(rest[0])
+    x_max = float(rest[1])
+    n_x = int(rest[2])
+    if debug:
+        print(f"{x_min} <= x <= {x_max}, n_x = {n_x}")
+
+    # Print the output header lines.
+    header = "# GRID"
+    print(header)
+    header = "# x"
+    print(header)
+    header = f"# {x_min} {x_max} {n_x}"
+    print(header)
+    header = "# x Ψ"
+    print(header)
+
+    # Compute the data for the boundary condition at x = 0.
+    # Each line is:
+    # x Ψ
+    x = 0.0
+    Ψ = p.Ψ_analytical(x)
+    print(x, Ψ)
+
+
+if __name__ == "__main__":
+    """Begin main program."""
+    main()
---
+++ problems/lagaris/lagaris03/lagaris03_pinn2_plots.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+
+"""Create plots for pinn2 results for lagaris03 problem.
+
+Create plots for pinn1 results for lagaris03 problem.
+
+Author
+------
+Eric Winter (eric.winter62@gmail.com)
+"""
+
+# Import standard modules.
+import argparse
+from importlib import import_module
+import os
+import sys
+
+# Import supplemental modules.
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+# Import project modules.
+import pinn.common
+
+
+# Program constants
+
+# Program description
+DESCRIPTION = "Create plots for pinn2 results for lagaris03 problem."
+
+# Name of directory to hold output plots
+OUTPUT_DIR = "pinn2_plots"
+
+# Name of problem
+PROBLEM_NAME = "lagaris03"
+
+# Number of points to use in comparison plot.
+NUM_POINTS = 101
+
+# Plot limits for dependent variables.
+ylim = {}
+ylim["L"] = [1e-12, 10]
+ylim["Ψ"] = [-1.0, 1.0]
+
+
+def create_command_line_argument_parser():
+    """Create the command-line argument parser.
+
+    Create the command-line argument parser.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    parser : argparse.ArgumentParser
+        Parser for command-line arguments.
+    """
+    parser = argparse.ArgumentParser(DESCRIPTION)
+    parser.add_argument(
+        "--debug", "-d", action="store_true",
+        help="Print debugging output (default: %(default)s)."
+    )
+    parser.add_argument(
+        "--verbose", "-v", action="store_true",
+        help="Print verbose output (default: %(default)s)."
+    )
+    parser.add_argument(
+        "results_path",
+        help="Path to directory containing results to plot."
+    )
+    return parser
+
+
+def main():
+    """Main program."""
+    # Set up the command-line parser.
+    parser = create_command_line_argument_parser()
+
+    # Parse the command-line arguments.
+    args = parser.parse_args()
+    debug = args.debug
+    verbose = args.verbose
+    results_path = args.results_path
+    if debug:
+        print(f"args = {args}", flush=True)
+
+    # Add the run results directory to the module search path.
+    sys.path.append(results_path)
+
+    # Import the problem definition from the run results directory.
+    p = import_module(PROBLEM_NAME)
+
+    # Compute the path to the output directory. Then create it if needed.
+    output_path = OUTPUT_DIR
+    os.mkdir(output_path)
+
+    # Create the plots in a memory buffer.
+    mpl.use("Agg")
+
+    # -------------------------------------------------------------------------
+
+    # Plot the total residual, data, and weighted loss histories.
+
+    # Load the data.
+    path = os.path.join(results_path, "L_res.dat")
+    L_res = np.loadtxt(path)
+    path = os.path.join(results_path, "L_data.dat")
+    L_dat = np.loadtxt(path)
+    path = os.path.join(results_path, "L.dat")
+    L = np.loadtxt(path)
+
+    # Create the plot.
+    plt.clf()
+    plt.semilogy(L_res, label="$L_{res}$")
+    plt.semilogy(L_dat, label="$L_{dat}$")
+    plt.semilogy(L, label="$L$")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.ylim(ylim["L"])
+    plt.legend()
+    plt.title(f"Total residual, data, and weighted loss")
+    plt.grid()
+
+    # Save the plot to a PNG file.
+    path = os.path.join(output_path, "L.png")
+    if verbose:
+        print(f"Saving {path}.")
+    plt.savefig(path)
+    plt.close()
+
+    # ------------------------------------------------------------------------
+
+    # Plot the per-model residual, data, and weighted loss histories.
+
+    # Plot for each model.
+    for iv in range(p.n_var):
+
+        # Load the data.
+        variable_name = p.dependent_variable_names[iv]
+        variable_label = p.dependent_variable_labels[iv]
+        path = os.path.join(results_path, f"L_res_{variable_name}.dat")
+        L_res = np.loadtxt(path)
+        path = os.path.join(results_path, f"L_data_{variable_name}.dat")
+        L_dat = np.loadtxt(path)
+        path = os.path.join(results_path, f"L_{variable_name}.dat")
+        L = np.loadtxt(path)
+
+        # Create the plot.
+        plt.semilogy(L_res, label="$L_{res}$")
+        plt.semilogy(L_dat, label="$L_{dat}$")
+        plt.semilogy(L, label="$L$")
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        plt.ylim(ylim["L"])
+        plt.legend()
+        plt.title(f"Residual, data, and weighted loss for {variable_label}")
+        plt.grid()
+
+        # Save the plot.
+        path = os.path.join(output_path, f"L_{variable_name}.png")
+        if verbose:
+            print(f"Saving {path}.")
+        plt.savefig(path)
+        plt.close()
+
+    # ------------------------------------------------------------------------
+
+    # Load the training points.
+    path = os.path.join(results_path, "X_train.dat")
+    X_train = np.loadtxt(path)
+
+    # Load the additional data.
+    path = os.path.join(results_path, "XY_data.dat")
+    XY_data = np.loadtxt(path)
+
+    # Read the data description from the header.
+    with open(path, "r") as f:
+        line = f.readline()
+        line = f.readline()
+        line = f.readline()
+        line = line[2:]
+        fields = line.split(" ")
+        xmin = float(fields[0])
+        xmax = float(fields[1])
+        nx = int(fields[2])
+
+    # Find the epoch of the last trained model.
+    last_epoch = pinn.common.find_last_epoch(results_path)
+
+    # Load the trained model for each variable.
+    models = []
+    for variable_name in p.dependent_variable_names:
+        path = os.path.join(results_path, "models", f"{last_epoch:06d}",
+                            f"model_{variable_name}")
+        model = tf.keras.models.load_model(path)
+        models.append(model)
+
+    # ------------------------------------------------------------------------
+
+    # Plot the predicted and analytical solutions, and error.
+    for (iv, variable_name) in enumerate(p.dependent_variable_names):
+        if verbose:
+            print(f"Creating plot for {variable_name}.")
+        xlabel = p.independent_variable_labels[p.ix]
+        ylabel = p.dependent_variable_labels[iv]
+        X = X_train
+        model = models[iv]
+        Ym = model(X_train).numpy().reshape(nx)
+        Ya = p.Ψ_analytical(X_train)
+        Ye = Ym - Ya
+        rms_err = np.sqrt(np.sum(Ye**2)/nx)
+        plt.plot(X, Ym, label="trained")
+        plt.plot(X, Ya, label="analytical")
+        plt.plot(X, Ye, label="error")
+        plt.ylim(ylim[variable_name])
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.grid()
+        plt.legend()
+        title = f"{ylabel}, RMS err = {rms_err:.2e}"
+        plt.title(title)
+        path = os.path.join(output_path, f"{variable_name}.png")
+        if verbose:
+            print(f"Saving {path}.")
+        plt.savefig(path)
+        plt.close()
+
+
+if __name__ == "__main__":
+    """Begin main program."""
+    main()
