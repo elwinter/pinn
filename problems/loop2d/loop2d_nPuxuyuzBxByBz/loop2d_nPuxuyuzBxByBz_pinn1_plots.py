107c91a67a5a6fd8873792f38254c82d0c6719eb
Eric Winter <eric.winter@jhuapl.edu>
2024-02-26 19:43:10 -0500

    Snapshot prior to merge.

--- problems/loop2d/loop2d_nPuxuyuzBxByBz/loop2d_nPuxuyuzBxByBz_pinn1_plots.py
+++ problems/loop2d/loop2d_nPuxuyuzBxByBz/loop2d_nPuxuyuzBxByBz_pinn1_plots.py
@@ -125,11 +125,11 @@ def main():
     path = os.path.join(results_path, "L_res.dat")
     L_res = np.loadtxt(path)
     path = os.path.join(results_path, "L_constraint.dat")
+    use_constraints = False
     L_constraint = None
     if os.path.exists(path):
         use_constraints = True
         L_constraint = np.loadtxt(path)
-    L = np.loadtxt(path)
     path = os.path.join(results_path, "L_data.dat")
     L_dat = np.loadtxt(path)
     path = os.path.join(results_path, "L.dat")
@@ -250,8 +250,13 @@ def main():
     # Plot the actual and predicted initial magnetic field vectors.
     B0x_act = XY_data[:n_start, p.n_dim + p.iBx].reshape(n_start)
     B0y_act = XY_data[:n_start, p.n_dim + p.iBy].reshape(n_start)
-    B0x_pred = models[p.iBx](txy0).numpy().reshape(n_start)
-    B0y_pred = models[p.iBy](txy0).numpy().reshape(n_start)
+    if multi:
+        pred = model(txy0).numpy()
+        B0x_pred = pred[:, p.iBx].reshape(n_start)
+        B0y_pred = pred[:, p.iBy].reshape(n_start)
+    else:
+        B0x_pred = models[p.iBx](txy0).numpy().reshape(n_start)
+        B0y_pred = models[p.iBy](txy0).numpy().reshape(n_start)
     title = "Magnetic field at t = 0"
     pinn.standard_plots.plot_actual_predicted_B(
         x0, y0, B0x_act, B0y_act, B0x_pred, B0y_pred, title=title
@@ -278,20 +283,44 @@ def main():
 
     # Plot parameters.
     plot_min = {
+        "n" : -1.0,
+        "P" : -1.0,
+        "ux": -1.0,
+        "uy": -1.0,
+        "uz": -1.0,
         "Bx": -5e-3,
         "By": -5e-3,
+        "Bz": -1.0,
     }
     plot_max = {
+        "n" : 1.0,
+        "P" : 1.0,
+        "ux": 1.0,
+        "uy": 1.0,
+        "uz": 1.0,
         "Bx": 5e-3,
         "By": 5e-3,
+        "Bz": 1.0,
     }
     plot_err_min = {
+        "n" : -1e-3,
+        "P" : -1e-3,
+        "ux": -1e-3,
+        "uy": -1e-3,
+        "uz": -1e-3,
         "Bx": -1e-3,
         "By": -1e-3,
+        "Bz": -1e-3,
     }
     plot_err_max = {
+        "n" : 1e-3,
+        "P" : 1e-3,
+        "ux": 1e-3,
+        "uy": 1e-3,
+        "uz": 1e-3,
         "Bx": 1e-3,
         "By": 1e-3,
+        "Bz": 1e-3,
     }
 
     # Create and save each frame.
@@ -323,7 +352,7 @@ def main():
             Ze = np.flip(Z_error[it, :].T, axis=0)
             pinn.standard_plots.plot_actual_predicted_error(
                 X, Y, Za, Zt, Ze,
-                title=f"{p.dependent_variable_labels[iv]}",
+                title=f"{p.dependent_variable_names[iv]}",
                 vmin=plot_min[variable_name], vmax=plot_max[variable_name],
                 err_vmin=plot_err_min[variable_name], err_vmax=plot_err_max[variable_name],
                 x_tick_pos=heatmap_x_tick_pos, x_tick_labels=heatmap_x_tick_labels,
@@ -347,182 +376,182 @@ def main():
         ]
         subprocess.run(args)
 
-    # ------------------------------------------------------------------------
+    # # ------------------------------------------------------------------------
 
-    # Make a movie of a quiver plot of the trained and analytical solutions.
+    # # Make a movie of a quiver plot of the trained and analytical solutions.
 
-    if verbose:
-        print("Creating movie for magnetic field.")
-    frame_dir = os.path.join(output_path, "frames_BxBy")
-    os.mkdir(frame_dir)
-    frames = []
-    for it in range(nt):
-        i0 = it*n_start
-        i1 = i0 + n_start
-        txy = tf.Variable(X_train[i0:i1, :])
-        t = X_train[i0:i1, p.it]
-        x = X_train[i0:i1, p.ix]
-        y = X_train[i0:i1, p.iy]
-        Bx_act = p.Bx_analytical(t, x, y)
-        By_act = p.By_analytical(t, x, y)
-        Bx_pred = models[p.iBx](txy).numpy().reshape(n_start)
-        By_pred = models[p.iBy](txy).numpy().reshape(n_start)
-        title = f"Magnetic field at t = {t[0]:.3e}"
-        pinn.standard_plots.plot_actual_predicted_B(
-            x, y, Bx_act, By_act, Bx_pred, By_pred, title=title
-        )
-        path = os.path.join(frame_dir, f"BxBy-{it:06}.png")
-        if verbose:
-            print(f"Saving {path}.")
-        plt.savefig(path)
-        frames.append(path)
-        plt.close()
+    # if verbose:
+    #     print("Creating movie for magnetic field.")
+    # frame_dir = os.path.join(output_path, "frames_BxBy")
+    # os.mkdir(frame_dir)
+    # frames = []
+    # for it in range(nt):
+    #     i0 = it*n_start
+    #     i1 = i0 + n_start
+    #     txy = tf.Variable(X_train[i0:i1, :])
+    #     t = X_train[i0:i1, p.it]
+    #     x = X_train[i0:i1, p.ix]
+    #     y = X_train[i0:i1, p.iy]
+    #     Bx_act = p.Bx_analytical(t, x, y)
+    #     By_act = p.By_analytical(t, x, y)
+    #     Bx_pred = models[p.iBx](txy).numpy().reshape(n_start)
+    #     By_pred = models[p.iBy](txy).numpy().reshape(n_start)
+    #     title = f"Magnetic field at t = {t[0]:.3e}"
+    #     pinn.standard_plots.plot_actual_predicted_B(
+    #         x, y, Bx_act, By_act, Bx_pred, By_pred, title=title
+    #     )
+    #     path = os.path.join(frame_dir, f"BxBy-{it:06}.png")
+    #     if verbose:
+    #         print(f"Saving {path}.")
+    #     plt.savefig(path)
+    #     frames.append(path)
+    #     plt.close()
 
-    # Assemble the frames into a movie.
-    frame_pattern = os.path.join(frame_dir, f"BxBy-%06d.png")
-    movie_file = os.path.join(output_path, "BxBy.mp4")
-    args = [
-        "ffmpeg", "-r", "10", "-s", "1920x1080",
-        "-i", frame_pattern,
-        "-vcodec", "libx264", "-crf", "25", "-pix_fmt", "yuv420p",
-        movie_file
-    ]
-    subprocess.run(args)
+    # # Assemble the frames into a movie.
+    # frame_pattern = os.path.join(frame_dir, f"BxBy-%06d.png")
+    # movie_file = os.path.join(output_path, "BxBy.mp4")
+    # args = [
+    #     "ffmpeg", "-r", "10", "-s", "1920x1080",
+    #     "-i", frame_pattern,
+    #     "-vcodec", "libx264", "-crf", "25", "-pix_fmt", "yuv420p",
+    #     movie_file
+    # ]
+    # subprocess.run(args)
 
-    # ------------------------------------------------------------------------
+    # # ------------------------------------------------------------------------
 
-    # Make a movie of the magnetic field intensity.
+    # # Make a movie of the magnetic field intensity.
 
-    # Plot parameters.
-    plot_min = {
-        "B": 0.0,
-    }
-    plot_max = {
-        "B": 5e-3,
-    }
-    plot_err_min = {
-        "B": -1e-3,
-    }
-    plot_err_max = {
-        "B": 1e-3,
-    }
+    # # Plot parameters.
+    # plot_min = {
+    #     "B": 0.0,
+    # }
+    # plot_max = {
+    #     "B": 5e-3,
+    # }
+    # plot_err_min = {
+    #     "B": -1e-3,
+    # }
+    # plot_err_max = {
+    #     "B": 1e-3,
+    # }
 
-    if verbose:
-        print("Creating movie for magnetic field intensity.")
-    frame_dir = os.path.join(output_path, "frames_B")
-    os.mkdir(frame_dir)
-    frames = []
-    for it in range(nt):
-        i0 = it*nx*ny
-        i1 = i0 + nx*ny
-        txy = tf.Variable(X_train[i0:i1, :])
-        t = X_train[i0:i1, p.it]
-        x = X_train[i0:i1, p.ix]
-        y = X_train[i0:i1, p.iy]
-        Bx_act = p.Bx_analytical(t, x, y)
-        By_act = p.By_analytical(t, x, y)
-        B_act = np.flip(np.sqrt(Bx_act**2 + By_act**2).reshape(nx, ny).T, axis=0)
-        Bx_pred = models[p.iBx](txy).numpy()
-        By_pred = models[p.iBy](txy).numpy()
-        B_pred = np.flip(np.sqrt(Bx_pred**2 + By_pred**2).reshape(nx, ny).T, axis=0)
-        B_err = B_pred - B_act
-        title = f"Magnetic field intensity at t = {t[0]:.3e}"
-        pinn.standard_plots.plot_actual_predicted_error(
-            x, y, B_act, B_pred, B_err,
-            title=title,
-            vmin=plot_min['B'], vmax=plot_max['B'],
-            err_vmin=plot_err_min['B'], err_vmax=plot_err_max['B'],
-            x_tick_pos=heatmap_x_tick_pos, x_tick_labels=heatmap_x_tick_labels,
-            y_tick_pos=heatmap_y_tick_pos, y_tick_labels=heatmap_y_tick_labels,
-        )
-        path = os.path.join(frame_dir, f"B-{it:06}.png")
-        if verbose:
-            print(f"Saving {path}.")
-        plt.savefig(path)
-        frames.append(path)
-        plt.close()
+    # if verbose:
+    #     print("Creating movie for magnetic field intensity.")
+    # frame_dir = os.path.join(output_path, "frames_B")
+    # os.mkdir(frame_dir)
+    # frames = []
+    # for it in range(nt):
+    #     i0 = it*nx*ny
+    #     i1 = i0 + nx*ny
+    #     txy = tf.Variable(X_train[i0:i1, :])
+    #     t = X_train[i0:i1, p.it]
+    #     x = X_train[i0:i1, p.ix]
+    #     y = X_train[i0:i1, p.iy]
+    #     Bx_act = p.Bx_analytical(t, x, y)
+    #     By_act = p.By_analytical(t, x, y)
+    #     B_act = np.flip(np.sqrt(Bx_act**2 + By_act**2).reshape(nx, ny).T, axis=0)
+    #     Bx_pred = models[p.iBx](txy).numpy()
+    #     By_pred = models[p.iBy](txy).numpy()
+    #     B_pred = np.flip(np.sqrt(Bx_pred**2 + By_pred**2).reshape(nx, ny).T, axis=0)
+    #     B_err = B_pred - B_act
+    #     title = f"Magnetic field intensity at t = {t[0]:.3e}"
+    #     pinn.standard_plots.plot_actual_predicted_error(
+    #         x, y, B_act, B_pred, B_err,
+    #         title=title,
+    #         vmin=plot_min['B'], vmax=plot_max['B'],
+    #         err_vmin=plot_err_min['B'], err_vmax=plot_err_max['B'],
+    #         x_tick_pos=heatmap_x_tick_pos, x_tick_labels=heatmap_x_tick_labels,
+    #         y_tick_pos=heatmap_y_tick_pos, y_tick_labels=heatmap_y_tick_labels,
+    #     )
+    #     path = os.path.join(frame_dir, f"B-{it:06}.png")
+    #     if verbose:
+    #         print(f"Saving {path}.")
+    #     plt.savefig(path)
+    #     frames.append(path)
+    #     plt.close()
 
-    # Assemble the frames into a movie.
-    frame_pattern = os.path.join(frame_dir, f"B-%06d.png")
-    movie_file = os.path.join(output_path, "B.mp4")
-    args = [
-        "ffmpeg", "-r", "10", "-s", "1920x1080",
-        "-i", frame_pattern,
-        "-vcodec", "libx264", "-crf", "25", "-pix_fmt", "yuv420p",
-        movie_file
-    ]
-    subprocess.run(args)
+    # # Assemble the frames into a movie.
+    # frame_pattern = os.path.join(frame_dir, f"B-%06d.png")
+    # movie_file = os.path.join(output_path, "B.mp4")
+    # args = [
+    #     "ffmpeg", "-r", "10", "-s", "1920x1080",
+    #     "-i", frame_pattern,
+    #     "-vcodec", "libx264", "-crf", "25", "-pix_fmt", "yuv420p",
+    #     movie_file
+    # ]
+    # subprocess.run(args)
 
-    # ------------------------------------------------------------------------
+    # # ------------------------------------------------------------------------
 
-    # Make a movie of the magnetic field divergence.
+    # # Make a movie of the magnetic field divergence.
 
-    # Plot parameters.
-    plot_min = {
-        "divB": -1e-3,
-    }
-    plot_max = {
-        "divB": 1e-3,
-    }
-    plot_err_min = {
-        "divB": -1e-3,
-    }
-    plot_err_max = {
-        "divB": 1e-3,
-    }
+    # # Plot parameters.
+    # plot_min = {
+    #     "divB": -1e-3,
+    # }
+    # plot_max = {
+    #     "divB": 1e-3,
+    # }
+    # plot_err_min = {
+    #     "divB": -1e-3,
+    # }
+    # plot_err_max = {
+    #     "divB": 1e-3,
+    # }
 
-    if verbose:
-        print("Creating movie for magnetic field divergence.")
-    frame_dir = os.path.join(output_path, "frames_divB")
-    os.mkdir(frame_dir)
-    frames = []
-    for it in range(nt):
-        i0 = it*nx*ny
-        i1 = i0 + nx*ny
-        txy = tf.Variable(X_train[i0:i1, :])
-        t = X_train[i0:i1, p.it]
-        x = X_train[i0:i1, p.ix]
-        y = X_train[i0:i1, p.iy]
-        dBx_dx_act = p.dBx_dx_analytical(t, x, y)
-        dBy_dy_act = p.dBy_dy_analytical(t, x, y)
-        divB_act = dBx_dx_act + dBy_dy_act
-        divB_act = divB_act.reshape(nx, ny)
-        divB_act = np.flip(divB_act.T, axis=0)
-        with tf.GradientTape(persistent=True) as tape1:
-            Bx_pred = models[p.iBx](txy)
-            By_pred = models[p.iBy](txy)
-        dBx_dx_pred = tape1.gradient(Bx_pred, txy)[:, p.ix].numpy()
-        dBy_dy_pred = tape1.gradient(Bx_pred, txy)[:, p.iy].numpy()
-        divB_pred = dBx_dx_pred + dBy_dy_pred
-        divB_pred = divB_pred.reshape(nx, ny)
-        divB_pred = np.flip(divB_pred.T, axis=0)
-        divB_err = divB_pred - divB_act
-        title = f"Magnetic field divergence at t = {t[0]:.3e}"
-        pinn.standard_plots.plot_actual_predicted_error(
-            x, y, divB_act, divB_pred, divB_err,
-            title=title,
-            vmin=plot_min['divB'], vmax=plot_max['divB'],
-            err_vmin=plot_err_min['divB'], err_vmax=plot_err_max['divB'],
-            x_tick_pos=heatmap_x_tick_pos, x_tick_labels=heatmap_x_tick_labels,
-            y_tick_pos=heatmap_y_tick_pos, y_tick_labels=heatmap_y_tick_labels,
-        )
-        path = os.path.join(frame_dir, f"divB-{it:06}.png")
-        if verbose:
-            print(f"Saving {path}.")
-        plt.savefig(path)
-        frames.append(path)
-        plt.close()
+    # if verbose:
+    #     print("Creating movie for magnetic field divergence.")
+    # frame_dir = os.path.join(output_path, "frames_divB")
+    # os.mkdir(frame_dir)
+    # frames = []
+    # for it in range(nt):
+    #     i0 = it*nx*ny
+    #     i1 = i0 + nx*ny
+    #     txy = tf.Variable(X_train[i0:i1, :])
+    #     t = X_train[i0:i1, p.it]
+    #     x = X_train[i0:i1, p.ix]
+    #     y = X_train[i0:i1, p.iy]
+    #     dBx_dx_act = p.dBx_dx_analytical(t, x, y)
+    #     dBy_dy_act = p.dBy_dy_analytical(t, x, y)
+    #     divB_act = dBx_dx_act + dBy_dy_act
+    #     divB_act = divB_act.reshape(nx, ny)
+    #     divB_act = np.flip(divB_act.T, axis=0)
+    #     with tf.GradientTape(persistent=True) as tape1:
+    #         Bx_pred = models[p.iBx](txy)
+    #         By_pred = models[p.iBy](txy)
+    #     dBx_dx_pred = tape1.gradient(Bx_pred, txy)[:, p.ix].numpy()
+    #     dBy_dy_pred = tape1.gradient(Bx_pred, txy)[:, p.iy].numpy()
+    #     divB_pred = dBx_dx_pred + dBy_dy_pred
+    #     divB_pred = divB_pred.reshape(nx, ny)
+    #     divB_pred = np.flip(divB_pred.T, axis=0)
+    #     divB_err = divB_pred - divB_act
+    #     title = f"Magnetic field divergence at t = {t[0]:.3e}"
+    #     pinn.standard_plots.plot_actual_predicted_error(
+    #         x, y, divB_act, divB_pred, divB_err,
+    #         title=title,
+    #         vmin=plot_min['divB'], vmax=plot_max['divB'],
+    #         err_vmin=plot_err_min['divB'], err_vmax=plot_err_max['divB'],
+    #         x_tick_pos=heatmap_x_tick_pos, x_tick_labels=heatmap_x_tick_labels,
+    #         y_tick_pos=heatmap_y_tick_pos, y_tick_labels=heatmap_y_tick_labels,
+    #     )
+    #     path = os.path.join(frame_dir, f"divB-{it:06}.png")
+    #     if verbose:
+    #         print(f"Saving {path}.")
+    #     plt.savefig(path)
+    #     frames.append(path)
+    #     plt.close()
 
-    # Assemble the frames into a movie.
-    frame_pattern = os.path.join(frame_dir, f"divB-%06d.png")
-    movie_file = os.path.join(output_path, "divB.mp4")
-    args = [
-        "ffmpeg", "-r", "10", "-s", "1920x1080",
-        "-i", frame_pattern,
-        "-vcodec", "libx264", "-crf", "25", "-pix_fmt", "yuv420p",
-        movie_file
-    ]
-    subprocess.run(args)
+    # # Assemble the frames into a movie.
+    # frame_pattern = os.path.join(frame_dir, f"divB-%06d.png")
+    # movie_file = os.path.join(output_path, "divB.mp4")
+    # args = [
+    #     "ffmpeg", "-r", "10", "-s", "1920x1080",
+    #     "-i", frame_pattern,
+    #     "-vcodec", "libx264", "-crf", "25", "-pix_fmt", "yuv420p",
+    #     movie_file
+    # ]
+    # subprocess.run(args)
 
 
 if __name__ == "__main__":
