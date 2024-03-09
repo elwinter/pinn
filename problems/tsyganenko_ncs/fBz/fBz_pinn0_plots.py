d3f0fb9a415cf6ae4ed9609505f7ace6559105d3
Eric Winter <eric.winter@jhuapl.edu>
2024-03-09 18:05:27 -0500

    Cleaned up and modified to use top-level module.

--- problems/tsyganenko_ncs/fBz/fBz_pinn0_plots.py
+++ problems/tsyganenko_ncs/fBz/fBz_pinn0_plots.py
@@ -28,21 +28,13 @@ import pinn.common
 # Program constants
 
 # Program description
-DESCRIPTION = "Create plots for pinn0 results for fBz problem."
+DESCRIPTION = 'Create plots for pinn0 results for fBz problem.'
 
 # Name of directory to hold output plots
-OUTPUT_DIR = "pinn0_plots"
+OUTPUT_DIR = 'pinn0_plots'
 
 # Name of problem
-PROBLEM_NAME = "fBz"
-
-# Number of points to use in comparison plot.
-NUM_POINTS = 101
-
-# Plot limits for dependent variables.
-ylim = {}
-ylim["L"] = [1e-3, 10]
-ylim["fBz"] = [-2.0, 2.0]
+PROBLEM_NAME = 'fBz'
 
 
 def create_command_line_argument_parser():
@@ -61,16 +53,20 @@ def create_command_line_argument_parser():
     """
     parser = argparse.ArgumentParser(DESCRIPTION)
     parser.add_argument(
-        "--debug", "-d", action="store_true",
+        '--debug', '-d', action="store_true",
         help="Print debugging output (default: %(default)s)."
     )
     parser.add_argument(
-        "--verbose", "-v", action="store_true",
+        '--verbose', '-v', action="store_true",
         help="Print verbose output (default: %(default)s)."
     )
     parser.add_argument(
-        "results_path",
-        help="Path to directory containing results to plot."
+        'results_path',
+        help='Path to directory containing results to plot.'
+    )
+    parser.add_argument(
+        'training_data_file',
+        help='Name of file in results_path which contains training data. The file must include a PINN grid definition header.'
     )
     return parser
 
@@ -87,14 +83,15 @@ def main():
     debug = args.debug
     verbose = args.verbose
     results_path = args.results_path
+    training_data_file = args.training_data_file
 
-    # Add the run results directory to the module search path.
-    sys.path.append(results_path)
+    # Add the run results directory at the head of the module search path.
+    sys.path.insert(0, results_path)
 
     # Import the problem definition from the run results directory.
     p = import_module(PROBLEM_NAME)
 
-    # Compute the path to the output directory. Then create it if needed.
+    # Compute the path to the output directory, then create it.
     output_path = OUTPUT_DIR
     os.mkdir(output_path)
 
@@ -104,86 +101,129 @@ def main():
     # -------------------------------------------------------------------------
 
     # Plot the loss history.
+    if verbose:
+        print(f"Plotting the loss history got {PROBLEM_NAME}.")
 
-    # Load the data.
-    path = os.path.join(results_path, "L_data.dat")
+    # Load the loss data.
+    path = os.path.join(results_path, 'L_data.dat')
     L_data = np.loadtxt(path)
 
+    # Specify figure settings.
+    figsize = (6.4, 4.8)  # This is the matplolib default.
+    nrows, ncols = 1, 1
+
+    # Create the figure.
+    fig = plt.figure(figsize=figsize)
+    fig.suptitle(
+        f"Loss function evolution for {p.dependent_variable_labels[p.ifBz]}"
+    )
+    gs = mpl.gridspec.GridSpec(nrows, ncols)
+
     # Create the plot.
-    plt.clf()
-    plt.semilogy(L_data, label="$L_{data}$")
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.ylim(ylim["L"])
-    plt.legend()
-    plt.title(f"Data loss")
-    plt.grid()
+    ax = fig.add_subplot(gs[0])
+    ax.grid()
+    ax.set_xlabel('Epoch')
+    ax.set_xlim([0, L_data.size])
+    ax.set_ylabel("$L$")
+    ax.set_ylim([1e-3, 10.0])
+    ax.grid(visible=True)
+
+    # Plot the data, then add the legend.
+    ax.semilogy(L_data, label="$L_{data}$")
+    ax.legend()
 
     # Save the plot to a PNG file.
-    path = os.path.join(output_path, "L_data.png")
+    path = os.path.join(output_path, 'L_data.png')
+    fig.savefig(path)
+    plt.close(fig)
     if verbose:
-        print(f"Saving {path}.")
-    plt.savefig(path)
-    plt.close()
+        print(f"Saved figure as {path}.")
 
     # ------------------------------------------------------------------------
 
     # Load the training points.
-    path = os.path.join(results_path, "fBz_011.dat")
-    XY_data = np.loadtxt(path)
+    path = os.path.join(results_path, training_data_file)
+    training_data = np.loadtxt(path)
 
     # Read the data description from the header.
-    with open(path, "r") as f:
-        line = f.readline()
-        line = f.readline()
-        line = f.readline()
-        line = line[2:]
-        fields = line.split(" ")
+    with open(path, 'r', encoding='utf-8') as f:
+        line = f.readline()  # GRID
+        line = f.readline()  # Bz
+        line = f.readline()  # Bzmin Bzmax nBz
+        line = line[2:].rstrip()
+        fields = line.split(' ')
         Bzmin = float(fields[0])
         Bzmax = float(fields[1])
         nBz = int(fields[2])
 
     # Find the epoch of the last trained model.
-    last_epoch = pinn.common.find_last_epoch(results_path)
+    last_model_epoch = pinn.common.find_last_epoch(results_path)
 
     # Load the trained model for each variable.
     models = []
     for variable_name in p.dependent_variable_names:
-        path = os.path.join(results_path, "models", f"{last_epoch:06d}",
+        path = os.path.join(results_path, 'models', f"{last_model_epoch:06d}",
                             f"model_{variable_name}")
         model = tf.keras.models.load_model(path)
         models.append(model)
 
     # ------------------------------------------------------------------------
 
-    # Plot the predicted and analytical solutions, error, and training points.
+    # Plot the predicted and empirical solutions, and error, in a single
+    # figure.
+    variable_name = p.dependent_variable_names[p.ifBz]
     if verbose:
-            print(f"Creating plot for {variable_name}.")
-    xlabel = p.independent_variable_labels[p.iBz]
-    ylabel = p.dependent_variable_labels[p.ifBz]
-    Bz = XY_data[:, 0]
-    model = models[0]
-    fBz_model = model(Bz).numpy().reshape(nBz)
-    fBz_analytical = p.fBz_analytical(Bz)
-    fBz_err = fBz_model - fBz_analytical
-    rms_err = np.sqrt(np.sum(fBz_err**2)/nBz)
-    plt.plot(Bz, fBz_model, label="trained")
-    plt.plot(Bz, fBz_analytical, label="analytical")
-    plt.plot(Bz, fBz_err, label="error")
-    plt.ylim(ylim["fBz"])
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.grid()
-    plt.legend()
-    title = f"{ylabel}, RMS err = {rms_err:.2e}"
-    plt.title(title)
-    path = os.path.join(output_path, "fBz.png")
+        print(f"Creating predicted/empirical/error figure for {variable_name}.")
+
+    # Extract the training points, then compute the trained and empirical
+    # solutions, and error.
+    Bz_train = training_data[:, 0]
+    fBz_trained = model(Bz_train).numpy().reshape(Bz_train.shape)
+    fBz_empirical = p.fBz_empirical(Bz_train)
+    fBz_error = fBz_trained - fBz_empirical
+
+    # Compute the RMS error.
+    fBz_rmserr = np.sqrt(np.sum(fBz_error**2)/fBz_error.size)
+
+    # Specify figure settings.
+    figsize = (6.4, 4.8)  # This is the matplolib default.
+    nrows, ncols = 1, 1
+
+    # Create the figure.
+    fig = plt.figure(figsize=figsize)
+    fig.suptitle(
+        "Comparison of trained and empirical solutions for $f_{Bz}$"
+    )
+    gs = mpl.gridspec.GridSpec(nrows, ncols)
+
+    # Create the plot.
+    ax = fig.add_subplot(gs[0])
+    ax.grid()
+    ax.set_xlabel("$B_z$")
+    ax.set_xlim([Bzmin, Bzmax])
+    ax.set_ylabel("$f_{Bz}$")
+    fBzmin, fBzmax = -2.0, 2.0
+    ax.set_ylim([fBzmin, fBzmax])
+    ax.grid(visible=True)
+
+    # Plot the data, then add the legend.
+    ax.plot(Bz_train, fBz_trained, label="$f_{Bz}$ (trained)")
+    ax.plot(Bz_train, fBz_empirical, label="$f_{Bz}$ (empirical)")
+    ax.plot(Bz_train, fBz_error, label="$f_{Bz}$ error")
+    ax.legend()
+
+    # Add a plot title with the RMS error.
+    text = f"RMS error = {fBz_rmserr:.2E}"
+    ax.set_title(text)
+
+    # Save the plot to a PNG file.
+    path = os.path.join(output_path, 'fBz.png')
+    fig.savefig(path)
+    plt.close(fig)
     if verbose:
-        print(f"Saving {path}.")
-    plt.savefig(path)
-    plt.close()
+        print(f"Saved figure as {path}.")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """Begin main program."""
     main()
