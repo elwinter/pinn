eb361f3d6f55e5e792ee7899651bd0b076c666e3
Eric Winter <eric.winter@jhuapl.edu>
2024-03-09 22:33:06 -0500

    Converted to use top-level module.

--- problems/tsyganenko_ncs/T/T_create_data.py
+++ problems/tsyganenko_ncs/T/T_create_data.py
@@ -21,7 +21,7 @@ import problems.tsyganenko_ncs.T.T as p
 # Program constants
 
 # Program description.
-description = "Create data for T problem."
+description = 'Create data for T problem.'
 
 
 def create_command_line_argument_parser():
@@ -44,10 +44,10 @@ def create_command_line_argument_parser():
     """
     parser = argparse.ArgumentParser(description)
     parser.add_argument(
-        "--debug", "-d", action="store_true",
+        '--debug', '-d', action='store_true',
         help="Print debugging output (default: %(default)s)."
     )
-    parser.add_argument("rest", nargs=argparse.REMAINDER)
+    parser.add_argument('rest', nargs=argparse.REMAINDER)
     return parser
 
 
@@ -64,35 +64,55 @@ def main():
     rest = args.rest
 
     # Fetch the remaining command-line arguments.
-    # They should be in a set of 3:
-    # fPmin fP_max nfP
-    assert len(rest) == 3
-    fPmn = float(rest[0])
+    # They should be in 3 sets of 3:
+    # fPmin fPmax nfP fBzmin fBzmax nfBz phimin phimax nphi
+    assert len(rest) == 9
+    fPmin = float(rest[0])
     fPmax = float(rest[1])
     nfP = int(rest[2])
+    fBzmin = float(rest[3])
+    fBzmax = float(rest[4])
+    nfBz = int(rest[5])
+    phimin = float(rest[6])
+    phimax = float(rest[7])
+    nphi = int(rest[8])
     if debug:
         print(f"{fPmin} <= fP <= {fPmax}, nfP = {nfP}")
+        print(f"{fBzmin} <= fBz <= {fBzmax}, nfBz = {nfBz}")
+        print(f"{phimin} <= phi <= {phimax}, nphi = {nphi}")
 
     # Print the output header lines.
-    header = "# GRID"
+    header = '# GRID'
     print(header)
-    header = "# T"
+    header = (
+        f"# {p.independent_variable_names[p.ifP]} "
+        f"{p.independent_variable_names[p.ifBz]} "
+        f"{p.independent_variable_names[p.iphi]}"
+    )
     print(header)
-    header = f"# {fPmin} {fPmax} {nfP}"
+    header = (
+        f"# {fPmin} {fPmax} {nfP} "
+        f"{fBzmin} {fBzmax} {nfBz} "
+        f"{phimin} {phimax} {nphi}"
+    )
     print(header)
-    header = "# fP T"
+    header = (
+        f"# {p.independent_variable_names[p.ifP]} "
+        f"{p.independent_variable_names[p.ifBz]} "
+        f"{p.independent_variable_names[p.iphi]} "
+        f"{p.dependent_variable_names[p.iT]}"
+    )
     print(header)
 
-    # Compute the data..
-    # Each line is:
-    # fP T
+    # Compute the data and send to stdout.
     fP = np.linspace(fPmin, fPmax, nfP)
-    T = p.T_analytical(fP)
-    for fp, t in zip(fP, T):
-        print(f"{fP} {T}")
+    fBz = np.linspace(fBzmin, fBzmax, nfBz)
+    phi = np.linspace(phimin, phimax, nphi)
+    T = p.T_empirical(fP, fBz, phi)
+    for _fP, _fBz, _phi, _T in zip(fP, fBz, phi, T):
+        print(f"{_fP} {_fBz}, {_phi} {_T}")
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     """Begin main program."""
     main()
