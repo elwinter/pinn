ebbbacc2d540302b74c0c9610d12eb17ce6cb746
Eric Winter <eric.winter@jhuapl.edu>
2025-04-07 13:16:41 -0400

    Now makes movies of PAE plots.

--- problems/loop2d/loop2d_nPuxuyuzBxByBz/loop2d_nPuxuyuzBxByBz_pinn1_plots.py
+++ problems/loop2d/loop2d_nPuxuyuzBxByBz/loop2d_nPuxuyuzBxByBz_pinn1_plots.py
@@ -15,6 +15,7 @@ import copy
 import importlib
 import os
 import shutil
+import subprocess
 import sys
 
 # Import supplemental modules.
@@ -49,11 +50,12 @@ PROBLEM_NAME = "loop2d_nPuxuyuzBxByBz"
 # Name of directory to hold output plots
 OUTPUT_DIR = "pinn1_plots"
 
-# # Plot limits for dependent variables.
-# ylim = {}
-# ylim["L"] = [1e-12, 10]
-# ylim["Bx"] = [-1.0, 1.0]
-# ylim["By"] = [-1.0, 1.0]
+# Movie parameters
+FRAME_RATE = "10"  # Frames per second
+FRAME_SIZE = "1920x1080"
+VIDEO_CODEC = "libx264"
+CONSTANT_RATE_FACTOR = "25"
+PIXEL_FORMAT = "yuv420p"
 
 
 def create_command_line_parser():
@@ -142,7 +144,7 @@ def create_loss_plot(L_res: np.ndarray, L_dat: np.ndarray,
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.legend()
-    plt.title(f"Residual, data, and weighted loss")
+    plt.title("Residual, data, and weighted loss")
     plt.grid()
 
     # Return the figure.
@@ -183,31 +185,32 @@ def create_PAE_plot(X: np.ndarray, Y: np.ndarray,
         nrows=1, ncols=3, sharey=True,
         figsize=[18.0, 6.0]
         )
+    axp, axa, axe = axs
 
     # Predicted
-    ax = axs[0]
-    ax.pcolormesh(X, Y, P)
-    ax.set_aspect("equal")
-    ax.set_title("Predicted")
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.grid(True)
+    pcmp = axp.pcolormesh(X, Y, P)
+    axs[0].set_aspect("equal")
+    axs[0].set_title("Predicted")
+    axs[0].set_xlabel("x")
+    axs[0].set_ylabel("y")
+    axs[0].grid(True)
+    fig.colorbar(pcmp, ax=axp, orientation="horizontal")
 
     # Analytical
-    ax = axs[1]
-    ax.pcolormesh(X, Y, A)
-    ax.set_aspect("equal")
-    ax.set_title("Analytical")
-    ax.set_xlabel("x")
-    ax.grid(True)
+    pcma = axa.pcolormesh(X, Y, A)
+    axs[1].set_aspect("equal")
+    axs[1].set_title("Analytical")
+    axs[1].set_xlabel("x")
+    axs[1].grid(True)
+    fig.colorbar(pcma, ax=axa, orientation="horizontal")
 
     # Error
-    ax = axs[2]
-    ax.pcolormesh(X, Y, E)
-    ax.set_aspect("equal")
-    ax.set_title("Error")
-    ax.set_xlabel("x")
-    ax.grid(True)
+    pcme = axe.pcolormesh(X, Y, E)
+    axs[2].set_aspect("equal")
+    axs[2].set_title("Error")
+    axs[2].set_xlabel("x")
+    axs[2].grid(True)
+    fig.colorbar(pcme, ax=axe, orientation="horizontal")
 
     # Decorate the figure.
     fig.suptitle("Predicted, analytical, and error")
@@ -333,7 +336,7 @@ def pinn1_plots(**kwargs) -> int:
     # Load the training points and description.
     path = os.path.join(results_path, "X_train.dat")
     X_train = np.loadtxt(path)
-    with open(path, "r") as f:
+    with open(path, "r", encoding="utf-8") as f:
         line = f.readline()  # Skip 1st line - contains "# GRID"
         line = f.readline()  # Grid description on this line
         line = line[2:]
@@ -408,139 +411,28 @@ def pinn1_plots(**kwargs) -> int:
 
             # Create the plot.
             fig = create_PAE_plot(X, Y, P, A, E)
-            fig.suptitle(f"{variable_name} predicted, analytical, and error")
+            fig.suptitle(f"{variable_label} predicted, analytical, and error")
 
             # Save the plot to a PNG file.
             path = os.path.join(pae_path, f"PAE_{it:04d}_{variable_name}.png")
             fig.savefig(path)
             plt.close(fig)
 
-#     # Load the additional data (boundary and initial conditions).
-#     path = os.path.join(results_path, "XY_data.dat")
-#     XY_data = np.loadtxt(path)
+        # Assemble the frames into a movie.
+        # frame_pattern = os.path.join(pae_path, f"{variable_name}-%06d.png")
+        frame_pattern = os.path.join(pae_path, f"PAE_%04d_{variable_name}.png")
+        movie_file = os.path.join(pae_path, f"{variable_name}.mp4")
+        args = [
+            "ffmpeg", "-r", FRAME_RATE, "-s", FRAME_SIZE,
+            "-i", frame_pattern, "-vcodec", VIDEO_CODEC,
+            "-crf", CONSTANT_RATE_FACTOR, "-pix_fmt", PIXEL_FORMAT,
+            movie_file
+        ]
+        subprocess.run(args, check=True)
 
-#     # ------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
 
-#     # Plot the initial conditions as supplied to the models.
-
-#     # Extract the coordinates of the training points at the initial time.
-#     n_start = nx*ny
-#     txy0 = tf.Variable(XY_data[:n_start, :p.n_dim])
-#     x0 = txy0[:, p.ix].numpy()
-#     y0 = txy0[:, p.iy].numpy()
-
-#     # Plot the actual and predicted initial magnetic field vectors.
-#     B0x_act = XY_data[:n_start, p.n_dim + p.iBx].reshape(n_start)
-#     B0y_act = XY_data[:n_start, p.n_dim + p.iBy].reshape(n_start)
-#     if multi:
-#         pred = model(txy0).numpy()
-#         B0x_pred = pred[:, p.iBx].reshape(n_start)
-#         B0y_pred = pred[:, p.iBy].reshape(n_start)
-#     else:
-#         B0x_pred = models[p.iBx](txy0).numpy().reshape(n_start)
-#         B0y_pred = models[p.iBy](txy0).numpy().reshape(n_start)
-#     B0x_err = B0x_pred - B0x_act
-#     B0y_err = B0y_pred - B0y_act
-#     B0x_rms = np.sqrt(np.sum(B0x_err**2)/n_start)
-#     B0y_rms = np.sqrt(np.sum(B0y_err**2)/n_start)
-#     title = f"Magnetic field at t = 0, (Bx, By)_rms = ({B0x_rms:.2e}, {B0y_rms:.2e})"
-#     pinn.standard_plots.plot_actual_predicted_B(
-#         x0, y0, B0x_act, B0y_act, B0x_pred, B0y_pred, title=title
-#     )
-#     path = os.path.join(output_path, "B0_act_pred.png")
-#     if verbose:
-#         print(f"Saving {path}.")
-#     plt.savefig(path)
-#     plt.close()
-
-#     # ------------------------------------------------------------------------
-
-#     # # Make a movie for each predicted variable. Include the analytical solution
-#     # # and the error.
-
-#     # # Compute the heat map tick locations and labels.
-#     # HEATMAP_N_X_TICKS = 5
-#     # heatmap_x_tick_pos = np.linspace(0, nx - 1, HEATMAP_N_X_TICKS)
-#     # heatmap_x_tick_labels = ["%.1f" % (xmin + x/(nx - 1)*(xmax - xmin)) for x in heatmap_x_tick_pos]
-#     # HEATMAP_N_Y_TICKS = 5
-#     # heatmap_y_tick_pos = np.linspace(0, ny - 1, HEATMAP_N_Y_TICKS)
-#     # heatmap_y_tick_labels = ["%.1f" % (ymin + y/(ny - 1)*(ymax - ymin)) for y in heatmap_y_tick_pos]
-#     # heatmap_y_tick_labels = list(reversed(heatmap_y_tick_labels))
-
-#     # # Plot parameters.
-#     # plot_min = {
-#     #     "Bx": -5e-3,
-#     #     "By": -5e-3,
-#     # }
-#     # plot_max = {
-#     #     "Bx": 5e-3,
-#     #     "By": 5e-3,
-#     # }
-#     # plot_err_min = {
-#     #     "Bx": -1e-3,
-#     #     "By": -1e-3,
-#     # }
-#     # plot_err_max = {
-#     #     "Bx": 1e-3,
-#     #     "By": 1e-3,
-#     # }
-
-#     # # Create and save each frame.
-#     # for (iv, variable_name) in enumerate(p.dependent_variable_names):
-#     #     if verbose:
-#     #         print(f"Creating movie for {variable_name}.")
-#     #     xlabel = p.independent_variable_labels[p.ix]
-#     #     ylabel = p.independent_variable_labels[p.iy]
-#     #     frame_dir = os.path.join(output_path, f"frames_{variable_name}")
-#     #     os.mkdir(frame_dir)
-#     #     if multi:
-#     #         model = models[0]
-#     #         Z_trained = model(X_train).numpy()[:, iv].reshape(nt, nx, ny)
-#     #     else:
-#     #         model = models[iv]
-#     #         Z_trained = model(X_train).numpy().reshape(nt, nx, ny)
-#     #     Z_analytical = p.analytical_solutions[iv](
-#     #         X_train[:, p.it], X_train[:, p.ix], X_train[:, p.iy]).reshape(nt, nx, ny)
-#     #     Z_error = Z_trained - Z_analytical
-#     #     frames = []
-#     #     for it in range(nt):
-#     #         i0 = it*nx*ny
-#     #         i1 = i0 + nx*ny
-#     #         X = X_train[i0:i1, p.ix]
-#     #         Y = X_train[i0:i1, p.iy]
-#     #         # To get the proper orientation, reshape, transpose, flip.
-#     #         Zt = np.flip(Z_trained[it, :].T, axis=0)
-#     #         Za = np.flip(Z_analytical[it, :].T, axis=0)
-#     #         Ze = np.flip(Z_error[it, :].T, axis=0)
-#     #         pinn.standard_plots.plot_actual_predicted_error(
-#     #             X, Y, Za, Zt, Ze,
-#     #             title=f"{p.dependent_variable_names[iv]}",
-#     #             vmin=plot_min[variable_name], vmax=plot_max[variable_name],
-#     #             err_vmin=plot_err_min[variable_name], err_vmax=plot_err_max[variable_name],
-#     #             x_tick_pos=heatmap_x_tick_pos, x_tick_labels=heatmap_x_tick_labels,
-#     #             y_tick_pos=heatmap_y_tick_pos, y_tick_labels=heatmap_y_tick_labels,
-#     #         )
-#     #         path = os.path.join(frame_dir, f"{variable_name}-{it:06}.png")
-#     #         if verbose:
-#     #             print(f"Saving {path}.")
-#     #         plt.savefig(path)
-#     #         frames.append(path)
-#     #         plt.close()
-
-#     #     # Assemble the frames into a movie.
-#     #     frame_pattern = os.path.join(frame_dir, f"{variable_name}-%06d.png")
-#     #     movie_file = os.path.join(output_path, f"{variable_name}.mp4")
-#     #     args = [
-#     #         "ffmpeg", "-r", "10", "-s", "1920x1080",
-#     #         "-i", frame_pattern,
-#     #         "-vcodec", "libx264", "-crf", "25", "-pix_fmt", "yuv420p",
-#     #         movie_file
-#     #     ]
-#     #     subprocess.run(args)
-
-#     # # ------------------------------------------------------------------------
-
-#     # # Make a movie of a quiver plot of the trained and analytical solutions.
+    # Make a movie of the magnetic field vectors.
 
 #     # if verbose:
 #     #     print("Creating movie for magnetic field.")
@@ -580,9 +472,9 @@ def pinn1_plots(**kwargs) -> int:
 #     # ]
 #     # subprocess.run(args)
 
-#     # # ------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
 
-#     # # Make a movie of the magnetic field intensity.
+    # Make a movie of the magnetic field intensity.
 
 #     # # Plot parameters.
 #     # plot_min = {
@@ -644,9 +536,9 @@ def pinn1_plots(**kwargs) -> int:
 #     # ]
 #     # subprocess.run(args)
 
-#     # # ------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
 
-#     # # Make a movie of the magnetic field divergence.
+    # Make a movie of the magnetic field divergence.
 
 #     # # Plot parameters.
 #     # plot_min = {
@@ -717,6 +609,10 @@ def pinn1_plots(**kwargs) -> int:
 
     # ------------------------------------------------------------------------
 
+    # Make a movie of the magnetic energy.
+
+    # ------------------------------------------------------------------------
+
     # Return normally.
     return 0
 
@@ -757,5 +653,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    """Begin main program."""
     main()
