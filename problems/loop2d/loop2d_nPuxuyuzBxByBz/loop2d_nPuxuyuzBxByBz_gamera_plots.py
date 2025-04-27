15db2a94bca9120acf980232caa1e33318d551fb
Eric Winter <eric.winter@jhuapl.edu>
2025-04-27 10:06:17 -0400

    ncommented inactive code.

--- problems/loop2d/loop2d_nPuxuyuzBxByBz/loop2d_nPuxuyuzBxByBz_gamera_plots.py
+++ problems/loop2d/loop2d_nPuxuyuzBxByBz/loop2d_nPuxuyuzBxByBz_gamera_plots.py
@@ -644,101 +644,101 @@ def gamera_plots(**kwargs) -> int:
     )
     n_var = len(variable_names)
 
-    # # Create the predicted, analytical, and error movie for each variable.
-    # for iv in range(n_var):
-    #     variable_name = variable_names[iv]
-    #     variable_label = variable_labels[iv]
-    #     if verbose:
-    #         print(f"Creating PAE movie for {variable_name}.")
+    # Create the predicted, analytical, and error movie for each variable.
+    for iv in range(n_var):
+        variable_name = variable_names[iv]
+        variable_label = variable_labels[iv]
+        if verbose:
+            print(f"Creating PAE movie for {variable_name}.")
 
-    #     # Create a directory for the PAE plots for this variable.
-    #     pae_path = os.path.join(output_path, f"PAE_{variable_name}")
-    #     os.mkdir(pae_path)
+        # Create a directory for the PAE plots for this variable.
+        pae_path = os.path.join(output_path, f"PAE_{variable_name}")
+        os.mkdir(pae_path)
 
-    #     # Plot for each training grid time.
-    #     for it in range(nt):
+        # Plot for each training grid time.
+        for it in range(nt):
 
-    #         # Fetch the frame time and grid coordinates.
-    #         t = T[it, 0, 0]
-    #         _X = X[it].T
-    #         _Y = Y[it].T
+            # Fetch the frame time and grid coordinates.
+            t = T[it, 0, 0]
+            _X = X[it].T
+            _Y = Y[it].T
 
-    #         # To get the proper orientation, reshape, transpose.
-    #         P = predicted[variable_name][it].T
-    #         A = analytical[variable_name][it].T
-    #         E = error[variable_name][it].T
+            # To get the proper orientation, reshape, transpose.
+            P = predicted[variable_name][it].T
+            A = analytical[variable_name][it].T
+            E = error[variable_name][it].T
 
-    #         # Create the plot.
-    #         fig = create_PAE_plot(_X, _Y, P, A, E)
+            # Create the plot.
+            fig = create_PAE_plot(_X, _Y, P, A, E)
 
-    #         # Tweak the frame title and error plot title.
-    #         fig.suptitle(f"{variable_label}, t = {t:.2E} predicted, "
-    #                      "analytical, and error "
-    #                      f"(overall RMS={RMS[variable_name]:.2E})")
-    #         fig.axes[2].set_title("Error (RMS = "
-    #                               f"{rms[variable_name][it]:.2E})")
+            # Tweak the frame title and error plot title.
+            fig.suptitle(f"{variable_label}, t = {t:.2E} predicted, "
+                         "analytical, and error "
+                         f"(overall RMS={RMS[variable_name]:.2E})")
+            fig.axes[2].set_title("Error (RMS = "
+                                  f"{rms[variable_name][it]:.2E})")
 
-    #         # Save the plot to a PNG file.
-    #         path = os.path.join(pae_path, f"PAE_{variable_name}_{it:04d}.png")
-    #         fig.savefig(path)
-    #         plt.close(fig)
+            # Save the plot to a PNG file.
+            path = os.path.join(pae_path, f"PAE_{variable_name}_{it:04d}.png")
+            fig.savefig(path)
+            plt.close(fig)
 
-    #     # Assemble the frames into a movie.
-    #     frame_pattern = os.path.join(pae_path, f"PAE_{variable_name}_%04d.png")
-    #     movie_file = os.path.join(pae_path, f"PAE_{variable_name}.mp4")
-    #     assemble_movie(frame_pattern, movie_file)
+        # Assemble the frames into a movie.
+        frame_pattern = os.path.join(pae_path, f"PAE_{variable_name}_%04d.png")
+        movie_file = os.path.join(pae_path, f"PAE_{variable_name}.mp4")
+        assemble_movie(frame_pattern, movie_file)
 
     # ------------------------------------------------------------------------
 
-    # # Make a PA movie of the magnetic field vectors.
-    # if verbose:
-    #     print("Creating PA movie for magnetic field.")
-    # pa_path = os.path.join(output_path, "PA_BxBy")
-    # os.mkdir(pa_path)
+    # Make a PA movie of the magnetic field vectors.
+    if verbose:
+        print("Creating PA movie for magnetic field.")
+    pa_path = os.path.join(output_path, "PA_BxBy")
+    os.mkdir(pa_path)
 
-    # # Compute the predicted and analytical magnetic field components.
-    # Bxp = models[p.iBx](X_train).numpy().reshape(nt, nx, ny)
-    # Byp = models[p.iBy](X_train).numpy().reshape(nt, nx, ny)
-    # Bxa = p.analytical_solutions[p.iBx](
-    #         X_train[:, p.it], X_train[:, p.ix], X_train[:, p.iy]
-    #     ).reshape(nt, nx, ny)
-    # Bya = p.analytical_solutions[p.iBy](
-    #         X_train[:, p.it], X_train[:, p.ix], X_train[:, p.iy]
-    #     ).reshape(nt, nx, ny)
+    # Compute the predicted and analytical magnetic field components.
+    Bxp = models[p.iBx](X_train).numpy().reshape(nt, nx, ny)
+    Byp = models[p.iBy](X_train).numpy().reshape(nt, nx, ny)
+    Bxa = p.analytical_solutions[p.iBx](
+            X_train[:, p.it], X_train[:, p.ix], X_train[:, p.iy]
+        ).reshape(nt, nx, ny)
+    Bya = p.analytical_solutions[p.iBy](
+            X_train[:, p.it], X_train[:, p.ix], X_train[:, p.iy]
+        ).reshape(nt, nx, ny)
 
-    # # Plot the field at each time.
-    # for it in range(nt):
+    # Plot the field at each time.
+    for it in range(nt):
 
-    #     # Compute the starting and ending index for this time.
-    #     i0 = it*nx*ny
-    #     i1 = i0 + nx*ny
+        # Compute the starting and ending index for this time.
+        i0 = it*nx*ny
+        i1 = i0 + nx*ny
 
-    #     # Fetch the frame time.
-    #     t = X_train[i0, p.it]
+        # Fetch the frame time.
+        t = X_train[i0, p.it]
 
-    #     # Extract the X and Y values for this time.
-    #     X = X_train[i0:i1, p.ix].reshape(nx, ny).T
-    #     Y = X_train[i0:i1, p.iy].reshape(nx, ny).T
+        # Extract the X and Y values for this time.
+        X = X_train[i0:i1, p.ix].reshape(nx, ny).T
+        Y = X_train[i0:i1, p.iy].reshape(nx, ny).T
 
-    #     # To get the proper orientation, reshape, transpose.
-    #     Px = Bxp[it, :].T
-    #     Py = Byp[it, :].T
-    #     Ax = Bxa[it, :].T
-    #     Ay = Bya[it, :].T
+        # To get the proper orientation, reshape, transpose.
+        Px = Bxp[it, :].T
+        Py = Byp[it, :].T
+        Ax = Bxa[it, :].T
+        Ay = Bya[it, :].T
 
-    #     # Create the plot.
-    #     fig = create_PA_BxBy_plot(X, Y, Px, Py, Ax, Ay)
-    #     fig.suptitle(f"Magnetic field, t = {t:.2E} predicted, analytical")
+        # Create the plot.
+        fig = create_PA_BxBy_plot(X, Y, Px, Py, Ax, Ay)
+        fig.suptitle(f"Magnetic field, t = {t:.2E} predicted, analytical")
 
-    #     # Save the plot to a PNG file.
-    #     path = os.path.join(pa_path, f"PA_BxBy_{it:04d}.png")
-    #     fig.savefig(path)
-    #     plt.close(fig)
+        # Save the plot to a PNG file.
+        path = os.path.join(pa_path, f"PA_BxBy_{it:04d}.png")
+        fig.savefig(path)
+        plt.close(fig)
 
-    # # Assemble the frames into a movie.
-    # frame_pattern = os.path.join(pa_path, "PA_BxBy_%04d.png")
-    # movie_file = os.path.join(pa_path, "PA_BxBy.mp4")
-    # assemble_movie(frame_pattern, movie_file)
+    # Assemble the frames into a movie.
+    frame_pattern = os.path.join(pa_path, "PA_BxBy_%04d.png")
+    movie_file = os.path.join(pa_path, "PA_BxBy.mp4")
+    assemble_movie(frame_pattern, movie_file)
 
     # ------------------------------------------------------------------------
 
